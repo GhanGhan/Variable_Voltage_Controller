5b5290b6dad889212fb4ce3ae355ac402ee8adaf
GhanGhan <ghanlawal@gmail.com>
2025-08-16 20:53:57 -0400

    Corrected Pic assigments in MX file (switched Chip select and reset pins), began writing program for VVC - Can turn the LCD screen on and off

---
+++ Program/VVC/Core/Inc/fonts.h
@@ -0,0 +1,16 @@
+/*
+ * fonts.h
+ *
+ *  Created on: Aug 13, 2025
+ *      Author: Ghani
+ */
+
+#ifndef INC_FONTS_H_
+#define INC_FONTS_H_
+
+#include <stdint.h>
+
+extern const uint8_t Ascii_1[97][5];
+
+
+#endif /* INC_FONTS_H_ */
