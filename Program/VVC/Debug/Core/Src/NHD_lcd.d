ee0b00216b4a0bb70e7360b9725bfee063a0a4c0
GhanGhan <ghanlawal@gmail.com>
2025-10-21 21:09:45 -0400

    Added and updated comments to all functions in NHD driver, and replaced &SPI_HANDLE with NHD_handle->hspi in HAL_SPI_Transmit function, don't know how it was working before

--- Program/VVC/Debug/Core/Src/NHD_lcd.d
+++ Program/VVC/Debug/Core/Src/NHD_lcd.d
@@ -1,4 +1,5 @@
-Core/Src/NHD_lcd.o: ../Core/Src/NHD_lcd.c ../Core/Inc/main.h \
+Core/Src/NHD_lcd.o: ../Core/Src/NHD_lcd.c ../Core/Inc/fonts.h \
+ ../Core/Inc/NHD_lcd.h \
  ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal.h \
  ../Core/Inc/stm32g0xx_hal_conf.h \
  ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_rcc.h \
@@ -32,9 +33,9 @@ Core/Src/NHD_lcd.o: ../Core/Src/NHD_lcd.c ../Core/Inc/main.h \
  ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_pwr_ex.h \
  ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_spi.h \
  ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_spi_ex.h \
- ../Core/Inc/fonts.h ../Core/Inc/NHD_lcd.h ../Core/Inc/NHD_lcd_config.h \
- ../Core/Inc/main.h
-../Core/Inc/main.h:
+ ../Core/Inc/NHD_lcd_config.h
+../Core/Inc/fonts.h:
+../Core/Inc/NHD_lcd.h:
 ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal.h:
 ../Core/Inc/stm32g0xx_hal_conf.h:
 ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_rcc.h:
@@ -68,7 +69,4 @@ Core/Src/NHD_lcd.o: ../Core/Src/NHD_lcd.c ../Core/Inc/main.h \
 ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_pwr_ex.h:
 ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_spi.h:
 ../Drivers/STM32G0xx_HAL_Driver/Inc/stm32g0xx_hal_spi_ex.h:
-../Core/Inc/fonts.h:
-../Core/Inc/NHD_lcd.h:
 ../Core/Inc/NHD_lcd_config.h:
-../Core/Inc/main.h:
